5c09971c4466c75508c0bb4e74d9340fa3bcf99a
Mario <mariohdpz@gmail.com>
2021-07-19 21:19:16 -0500

    Server configuration

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hola mundo 2");
+/* Adoptapet bootstrap */
+
+const express = require('express');
+const cors = require('cors');
+
+// Global app object
+const app = express();
+
+// Middleware config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Handling not found errors
+app.use(function (req, res, next) {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+// Bootstrap
+const server = app.listen(process.env.PORT || 3000, function () {
+    console.log(`App running. Listening on port ${server.address().port}`)
+});
